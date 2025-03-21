38da7107f0aebe5b6ea6ea6ff61987401b850926
Bohdan <b.yaronnyi@gmail.com>
2025-03-21 18:49:36 +0200

    update version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
 var swiper = new Swiper(".ourServiceSlider", {
-    slidesPerView: 4.5,
+    slidesPerView: 1.5,
     spaceBetween: 30,
     // loop: true,
     // autoplay: {
     //   delay: 4000,
     // },
+    breakpoints: {
+      // when window width is >= 320px
+   
+      // when window width is >= 480px
+      768: {
+          slidesPerView: 3.5,
+      },
+      // when window width is >= 640px
+      991: {
+          slidesPerView: 4.5,
+      }
+  }
   });
 
 
