27f18bc3c1024e9b62699e94acbb346c34b09a24
Bohdan <b.yaronnyi@gmail.com>
2025-03-21 16:38:21 +0200

    update version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,24 @@
 var swiper = new Swiper(".ourServiceSlider", {
-    slidesPerView: 3.5,
+    slidesPerView: 4.5,
     spaceBetween: 30,
-  
+    // loop: true,
+    // autoplay: {
+    //   delay: 4000,
+    // },
+  });
+
+
+  var swiper = new Swiper(".prewPictures", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
   });
